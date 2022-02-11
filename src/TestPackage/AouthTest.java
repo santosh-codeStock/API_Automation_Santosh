993503033632dbfdea42ad0162ca8ddb01368dc6
santosh <k.santosh51@yahoo.in>
2022-02-11 17:37:21 +0530

    aouth demo

--- src/TestPackage/AouthTest.java
+++ src/TestPackage/AouthTest.java
@@ -1,6 +1,10 @@
 package TestPackage;
 import static io.restassured.RestAssured.given;
 
+import java.util.List;
+
+import Pojo.GetCourse;
+import Pojo.WebAutomation;
 import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
 
@@ -8,7 +12,7 @@ public class AouthTest {
 
 	public static void main(String[] args) {
 		
-		String url="https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWg-4YRIRAoCkKwym1Qmt39DldkDNIn_blKApIcDmQQCIBtTyYYQ5NXqzss7g7sLaA&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none";
+		String url="https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWi1lUHfqldrmdd10VERPLFYXt2eVBbhVMvpRkfwePi0wLNRjCwBWuFC6mZD7M90ow&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none";
 		String code1=url.split("code=")[1];
 		String code2=code1.split("&scope=")[0];
 		System.out.println(code2);
@@ -17,9 +21,6 @@ public class AouthTest {
 		queryParams("code",code2).
 		queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com").
 		queryParams("client_secret","erZOWM9g3UtwNRj340YYaK_W").
-//		queryParams("state", "verifyfjdss").
-//		queryParams("session_state", "ff4a89d1f7011eb34eef8cf02ce4353316d9744b..7eb8").
-//		queryParams("scope", "https://www.googleapis.com/auth/userinfo.email").
 		queryParams("redirect_uri","https://rahulshettyacademy.com/getCourse.php").
 		queryParams("grant_type","authorization_code").
         when().log().all()
@@ -29,14 +30,27 @@ public class AouthTest {
 		JsonPath js=new JsonPath(ressponse);
 		String accessToken=js.getString("access_token");
 		
-		System.out.println(accessToken);
-
-		String courseList= given().contentType("application/json").
-		queryParams("access_token", accessToken).expect().defaultParser(Parser.JSON).
-		when().get("https://rahulshettyacademy.com/getCourse.php").asString();
-		System.out.println("^^^^^^^CourseList^^^^^^^^^^^"+courseList);
+		System.out.println("ACCESS TOKEN---> "+accessToken);
+//************without POJO*************
+//		String courseList= given().contentType("application/json").
+//		queryParams("access_token", accessToken).expect().defaultParser(Parser.JSON).
+//		when().get("https://rahulshettyacademy.com/getCourse.php").asString();
+//		System.out.println("^^^^^^^CourseList^^^^^^^^^^^"+courseList);
 		
-
+//***WITH POJO**********************************
+		GetCourse gc= given().contentType("application/json").
+				queryParams("access_token", accessToken).expect().defaultParser(Parser.JSON).
+				when().get("https://rahulshettyacademy.com/getCourse.php").as(GetCourse.class);
+		System.out.println("instructor name-> "+gc.getInstructor());
+//get value of nesteed json from response
+		gc.getCourses().getWebAutomation().get(1).getCourseTitle();
+	List<WebAutomation> listWebAuto=gc.getCourses().getWebAutomation();
+	for(int i=0;i<listWebAuto.size();i++)
+	{
+		System.out.print("WebAutomation course title & price -> "+listWebAuto.get(i).getCourseTitle()+" || "+listWebAuto.get(i).getPrice());
+		System.out.println();
+	}
+		
 	}
 
 }
